1d44ebd17cae33b62ca3a84a80fa184cb6344936
mariaranci <31917865+mariaranci@users.noreply.github.com>
2019-04-05 13:43:15 +0200

    Update Tclima_giorno.py

--- Tclima_giorno.py
+++ Tclima_giorno.py
@@ -37,19 +37,19 @@ def tabella():
         # massima
         df=pd.read_csv("Massima_81-10_" + siti[i][0] + ".txt", sep=',', names = ['data','misura',''], index_col = None, header=None)
         massima=df.misura[df['data']==DATACLIMA].to_string(index=False)
-        file_out.write(massima + '\n')
+        file_out.write('Massima Clima: ' + massima + '\n')
         # mimima
         df=pd.read_csv("Minima_81-10_" + siti[i][0] + ".txt", sep=',', names = ['data','misura',''], index_col = None, header=None)
         minima=df.misura[df['data']==DATACLIMA].to_string(index=False)
-        file_out.write(minima + '\n')
+        file_out.write('Minima Clima: ' + minima + '\n')
 
         #recupero da DBmeteo i dati di temperatura di ieri
         engine = create_engine('mysql+mysqldb://'+MYSQL_USER_ID+':'+MYSQL_USER_PWD+'@'+MYSQL_DB_HOST+'/'+MYSQL_DB_NAME)  
         conn=engine.connect()
         query="select  min(Misura) as Tmin, max(Misura) as Tmax from M_Termometri where IDsensore=" + str(siti[i][1]) + " and Data_e_ora>='"  + DATAI + "' and Data_e_ora<='" + DATAF + "'"
         result=pd.read_sql(query, conn)
-        file_out.write(result['Tmax'].to_string(index=False) + '\n')
-        file_out.write(result['Tmin'].to_string(index=False) + '\n')
+        file_out.write('Massima registrata ieri: ' + result['Tmax'].to_string(index=False) + '\n')
+        file_out.write('Minima registrata ieri: ' + result['Tmin'].to_string(index=False) + '\n')
 
      file_out.close() 
  
