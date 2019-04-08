a5d9c87750d4c7b1d96e47f517d28fc261a30786
mariaranci <31917865+mariaranci@users.noreply.github.com>
2019-04-08 18:05:18 +0200

    Update Tclima_giorno.py
    file json in output

--- Tclima_giorno.py
+++ Tclima_giorno.py
@@ -29,28 +29,31 @@ def tabella():
      #
      file_out = open('Tabella_Clima_' + DATAI + '.txt','w')
 
+     file_out.write('{"Temperature": { \n')
      siti = [["Milano", 5897], ["Pavia", 8157], ["Mantova", 5121],["Brescia", 2414],["Sondrio", 2096]]
      for i in range(len(siti)):
         # sito
-        file_out.write('\n\n' + str(siti[i][0]) + '\n')
+        if i >0 : file_out.write('     }, \n')
+        file_out.write('  "' + str(siti[i][0]) + '":{\n')
         # recupero dati da clima
         # massima
         df=pd.read_csv("Massima_81-10_" + siti[i][0] + ".txt", sep=',', names = ['data','misura',''], index_col = None, header=None)
         massima=df.misura[df['data']==DATACLIMA].to_string(index=False)
-        file_out.write('Massima Clima: ' + massima + '\n')
+        file_out.write('    "Tmax_clima":' + massima + ',\n')
         # mimima
         df=pd.read_csv("Minima_81-10_" + siti[i][0] + ".txt", sep=',', names = ['data','misura',''], index_col = None, header=None)
         minima=df.misura[df['data']==DATACLIMA].to_string(index=False)
-        file_out.write('Minima Clima: ' + minima + '\n')
+        file_out.write('    "Tmin_clima":' + minima + ',\n')
 
         #recupero da DBmeteo i dati di temperatura di ieri
         engine = create_engine('mysql+mysqldb://'+MYSQL_USER_ID+':'+MYSQL_USER_PWD+'@'+MYSQL_DB_HOST+'/'+MYSQL_DB_NAME)  
         conn=engine.connect()
         query="select  min(Misura) as Tmin, max(Misura) as Tmax from M_Termometri where IDsensore=" + str(siti[i][1]) + " and Data_e_ora>='"  + DATAI + "' and Data_e_ora<='" + DATAF + "'"
         result=pd.read_sql(query, conn)
-        file_out.write('Massima registrata ieri: ' + result['Tmax'].to_string(index=False) + '\n')
-        file_out.write('Minima registrata ieri: ' + result['Tmin'].to_string(index=False) + '\n')
-
+        file_out.write('    "Tmax":' + result['Tmax'].to_string(index=False) + ',\n')
+        file_out.write('    "Tmin":' + result['Tmin'].to_string(index=False) + '\n')
+     file_out.write('    }\n')
+     file_out.write('}}')
      file_out.close() 
  
 
