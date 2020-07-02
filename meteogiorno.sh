4aa76b8f23d3a32206d1640de7af0c9d801438c0
mariaranci <31917865+mariaranci@users.noreply.github.com>
2020-07-02 10:42:58 +0200

    Update meteogiorno.sh
    tolta produzione immagine da satellite

--- meteogiorno.sh
+++ meteogiorno.sh
@@ -2,22 +2,24 @@
 #=============================================================================
 # Lo script è all'interno di un container e produce i contenuti per il bollettino meteogiorno 
 #
-# ogni giorno:
+# ogni tre ore produce le seguenti mappe relative al giorno precedente:
 # -esegue uno script python che interroga il DBmeteo e postgresIRIS e produce la mappa dei pluviometri,  
 # -esegue uno script python che interroga il DBmeteo e i file climatici e produce la tabella clima
-# -esegue uno script pyton che recupera i wms e genera la immagine sinottica
+# (-esegue uno script pyton che recupera i wms e genera la immagine sinottica)
 # -carica gli output su Minio 
+# - cancella da Minio i file più vecchi di 5 giorni
 #
 # 2018/11/22 MR
 # 2019/03/20 MR GC UA aggiunta produzione tabella clima
 # 2019/04/09 aggiunta produzione immagine sinottica
+# 2020/07/02 rimossa produzione immagine sinottica 
 #=============================================================================
 
 export https_proxy="http://proxy2:8080"
 export http_proxy="http://proxy2:8080"
 
 #numsec=86400   # 60 * 60 * 24 -> 1 gg
-numsec=3600
+numsec=10800  # 3 ore
 
 S3CMD='s3cmd --config=config_minio.txt'
 
@@ -108,35 +110,35 @@ then
 
 
 ################### produzione immagine sinottica
- SINOTTICA_PY='sinottica.py'
- IMMAGINE_SINOTTICA='sinottica'$ieri'.png'
+#SINOTTICA_PY='sinottica.py'
+#IMMAGINE_SINOTTICA='sinottica'$ieri'.png'
     
-      python $SINOTTICA_PY
+#    python $SINOTTICA_PY
  
- # verifico se è andato a buon fine
-   STATO=$?
-   echo "STATO USCITA DA "$ $SINOTTICA_PY" ====> "$STATO
+# # verifico se è andato a buon fine
+#   STATO=$?
+#   echo "STATO USCITA DA "$ $SINOTTICA_PY" ====> "$STATO#
 
-   if [ "$STATO" -eq 1 ] # se si sono verificate anomalie esci 
-   then
-       exit 1
-   else # caricamento su MINIO 
-       putS3 . $IMMAGINE_SINOTTICA meteogiorno/ rete-monitoraggio 
+#   if [ "$STATO" -eq 1 ] # se si sono verificate anomalie esci 
+#   then
+#       exit 1
+#   else # caricamento su MINIO 
+#       putS3 . $IMMAGINE_SINOTTICA meteogiorno/ rete-monitoraggio 
 
-       # controllo sul caricamento su MINIO 
-       if [ $? -ne 0 ]
-       then
-         echo "problema caricamento su MINIO"
-         exit 1
-       fi
-   fi
+#       # controllo sul caricamento su MINIO 
+#       if [ $? -ne 0 ]
+#       then
+#         echo "problema caricamento su MINIO"
+#         exit 1
+#       fi
+#   fi
 
-   rm -f $IMMAGINE_SINOTTICA
+#   rm -f $IMMAGINE_SINOTTICA
    
    
    
   ################# pulizia cartella di minio
-  periodo="20 days"
+  periodo="5 days"
   $S3CMD --config=config_minio.txt ls s3://rete-monitoraggio/meteogiorno/ | while read -r line;
   do
     createDate=`echo $line|awk {'print $1'}`
