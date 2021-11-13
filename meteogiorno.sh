849dd7d4361878743b2bc76ef07a6768f73d917d
msalvati <m.salvati@arpalombardia.it>
2021-11-13 11:22:48 +0100

    aggiornato con copia su Nas e MYSQL_DB_PORT
    attenzione! le cartelle locali non sono un clone del repository quindi non si può usare GIT

--- meteogiorno.sh
+++ meteogiorno.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 #=============================================================================
-# Lo script è all'interno di un container e produce i contenuti per il bollettino meteogiorno 
+# Lo script è all'interno di un container e produce i contenuti per il bollettino meteogiorno
 #
 # ogni tre ore produce le seguenti mappe relative al giorno precedente:
-# -esegue uno script python che interroga il DBmeteo e postgresIRIS e produce la mappa dei pluviometri,  
+# -esegue uno script python che interroga il DBmeteo e postgresIRIS e produce la mappa dei pluviometri,
 # -esegue uno script python che interroga il DBmeteo e i file climatici e produce la tabella clima
 # (-esegue uno script pyton che recupera i wms e genera la immagine sinottica)
-# -carica gli output su Minio 
+# -carica gli output su Minio
 # - cancella da Minio i file più vecchi di 5 giorni
 #
 # 2018/11/22 MR
 # 2019/03/20 MR GC UA aggiunta produzione tabella clima
 # 2019/04/09 aggiunta produzione immagine sinottica
-# 2020/07/02 rimossa produzione immagine sinottica 
+# 2020/07/02 rimossa produzione immagine sinottica
+# 2021/10/14 sostituita copia su nasprevisore a copia su minio
 #=============================================================================
 
 export https_proxy="http://proxy2:8080"
 export http_proxy="http://proxy2:8080"
-export NO_PROXY="10.10.99.135"
+
+export IRIS_USER_ID=postgres
+export IRIS_USER_PWD=p0stgr3S
+export IRIS_DB_NAME=iris_base
+export IRIS_DB_HOST=10.10.0.19
+export MYSQL_USER_ID=guardone
+export MYSQL_USER_PWD=guardone
+export MYSQL_DB_NAME=METEO
+export MYSQL_DB_HOST=10.10.0.6
+export MYSQL_DB_PORT=3308
+
+nas_usr='ARPA\svcMeteo%woT$ln5=lProp1&w'
+NAS=//10.10.0.44/F
+putdir=PRECOMPILAZIONE/MeteoGIORNO
+fake=arpav-gagliard
 
 #numsec=86400   # 60 * 60 * 24 -> 1 gg
 numsec=10800  # 3 ore
@@ -26,33 +41,13 @@ S3CMD='s3cmd --config=config_minio.txt'
 
 SECONDS=$numsec
 
-putS3() {
-  path=$1
-  file=$2
-  aws_path=$3
-  bucket=$4
-  date=$(date -R)
-  acl="x-amz-acl:public-read"
-  content_type='application/x-compressed-tar'
-  string="PUT\n\n$content_type\n$date\n$acl\n/$bucket/$aws_path$file"
-  signature=$(echo -en "${string}" | openssl sha1 -hmac "${S3SECRET}" -binary | base64)
-  curl -X PUT -T "$path/$file" \
-    --progress-bar \
-    -H "Host: $S3HOST" \
-    -H "Date: $date" \
-    -H "Content-Type: $content_type" \
-    -H "$acl" \
-    -H "Authorization: AWS ${S3KEY}:$signature" \
-    "http://$S3HOST/$bucket/$aws_path$file"
-}
-
 #
 elapsed_time=$(date +%H)
 while [ 1 ]
 do
 # procedi sono se è passato numsec dall'ultimo invio
 if [[ ($elapsed_time -eq 07) || ($SECONDS -ge $numsec) ]]
-then 
+then
 
 
  ieri=$(date --date="yesterday" +"%Y-%m-%d")
@@ -61,22 +56,22 @@ then
  PLUVIO_GIORNO_PY='pluvio_giorno.py'
  FILE_PNG='pluvio_giorno_'$ieri'.png'
 
-    python $PLUVIO_GIORNO_PY 
+    python $PLUVIO_GIORNO_PY
 
    # verifico se è andato a buon fine
    STATO=$?
    echo "STATO USCITA DA "$ $PLUVIO_GIORNO_PY" ====> "$STATO
 
-   if [ "$STATO" -eq 1 ] # se si sono verificate anomalie esci 
+   if [ "$STATO" -eq 1 ] # se si sono verificate anomalie esci
    then
        exit 1
-   else # caricamento su MINIO 
-       putS3 . $FILE_PNG meteogiorno/ rete-monitoraggio 
+   else # caricamento su nasprevisore
+       smbclient -U $nas_usr $NAS -n $fake -c "prompt; cd ${putdir}; mput $FILE_PNG; quit"
 
-       # controllo sul caricamento su MINIO 
+       # controllo sul caricamento su nasprevisore
        if [ $? -ne 0 ]
        then
-         echo "problema caricamento su MINIO"
+         echo "problema caricamento su nasPrevisore"
          exit 1
        fi
    fi
@@ -85,24 +80,23 @@ then
 ################### produzione tabella clima
  CLIMA_GIORNO_PY='Tclima_giorno.py'
  FILE_TABELLA='Tabella_Clima_'$ieri'.json'
-    
+
       python $CLIMA_GIORNO_PY
- 
+
  # verifico se è andato a buon fine
    STATO=$?
    echo "STATO USCITA DA "$ $CLIMA_GIORNO_PY" ====> "$STATO
 
-   if [ "$STATO" -eq 1 ] # se si sono verificate anomalie esci 
+   if [ "$STATO" -eq 1 ] # se si sono verificate anomalie esci
    then
        exit 1
-   else # caricamento su MINIO 
-       putS3 . $FILE_TABELLA meteogiorno/ rete-monitoraggio 
+   else # caricamento su nasprevisore
+       smbclient -U $nas_usr $NAS -n $fake -c "prompt; cd ${putdir}; mput $FILE_TABELLA; quit"
 
-
-       # controllo sul caricamento su MINIO 
+       # controllo sul caricamento su nasprevisore
        if [ $? -ne 0 ]
        then
-         echo "problema caricamento su MINIO"
+         echo "problema caricamento su NASprevisore"
          exit 1
        fi
    fi
@@ -110,51 +104,31 @@ then
    rm -f $FILE_TABELLA
 
 
-################### produzione immagine sinottica
-#SINOTTICA_PY='sinottica.py'
-#IMMAGINE_SINOTTICA='sinottica'$ieri'.png'
-    
-#    python $SINOTTICA_PY
- 
-# # verifico se è andato a buon fine
-#   STATO=$?
-#   echo "STATO USCITA DA "$ $SINOTTICA_PY" ====> "$STATO#
 
-#   if [ "$STATO" -eq 1 ] # se si sono verificate anomalie esci 
-#   then
-#       exit 1
-#   else # caricamento su MINIO 
-#       putS3 . $IMMAGINE_SINOTTICA meteogiorno/ rete-monitoraggio 
 
-#       # controllo sul caricamento su MINIO 
-#       if [ $? -ne 0 ]
-#       then
-#         echo "problema caricamento su MINIO"
-#         exit 1
-#       fi
-#   fi
+  ################# pulizia cartella nasprevisore
 
-#   rm -f $IMMAGINE_SINOTTICA
-   
-   
-   
-  ################# pulizia cartella di minio
-  periodo="5 days"
-  $S3CMD --config=config_minio.txt ls s3://rete-monitoraggio/meteogiorno/ | while read -r line;
-  do
-    createDate=`echo $line|awk {'print $1'}`
-    createDate=`date -d"$createDate" +%s`
-    olderThan=`date -d"-$periodo" +%s`
-    if [[ $createDate -lt $olderThan ]]
-      then
-        fileName=`echo $line|awk {'print $4'}`
-        if [[ $fileName != "" ]]
-          then
-          $S3CMD del "$fileName"
-        fi
-    fi
-  done;
-  
+dataref=$[$(date +%j) - 1]
+
+# cancello file creati precedentemente a dataref
+
+    list=$(smbclient -U $nas_usr $NAS -n $fake -c "prompt; cd $putdir; ls {pluvio_*.png,Tabella_*.json}; quit" | \
+         awk '{ext="png"; ext2="json"; if (substr($1,length($1)-length(ext)+1,length(ext))==ext || substr($1,length($1)-length(ext2)+1,length(ext2))==ext2) print $1}')
+
+     for i in $list
+     do
+             echo $i >>  /opt/sat/bin/lista_elim_pluvio_e_clima.txt
+             detail=$(smbclient -U $nas_usr $NAS -n $fake -c "prompt; cd $putdir; allinfo $i; quit" | \
+               grep "create_time:" | awk 'BEGIN{FS=" "}{print $2,$3,$4,$7}')
+             datafile=$(date -d "$detail" +%j)
+             if [ $datafile -lt $dataref ]
+             then
+                     echo "$(date +%d"/"%m"/"%Y"/"%H":"%M) -> $i" >>  /opt/sat/bin/lista_elim_pluvio_e_clima.txt
+                     smbclient -U $nas_usr $NAS -n $fake -c "prompt; cd $putdir; del $i; quit"
+            fi
+     done
+
+
   SECONDS=0
   sleep $numsec
 fi
